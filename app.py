e300bd7590302c3e5c3e23dfee16532944575cda
Charles Chitty <charlescchitty@gmail.com>
2023-11-29 15:55:37 -0800

    Started prompt form generation

--- app.py
+++ app.py
@@ -7,3 +7,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def index():
+    """Return homepage"""
+
+    #Creates list ['noun', 'verb']
+    part_of_speeches= silly_story.prompts
+
+
+    html = render_template("questions.html", prompts = part_of_speeches)
+    print("html is: ", html)
+
+    return html
+
+#imports example story (ex: silly story instance)
+
+#Create a form where the inputs the user will make are on the prompts
+#taken from the silly story insance (ex:"noun")
