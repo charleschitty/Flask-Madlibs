eb9527ad1f7e1356261553263cd6e4f871a689a0
Carl Molina <carlandrew.molina@gmail.com>
2023-11-29 16:49:48 -0800

    After first code review

--- app.py
+++ app.py
@@ -12,11 +12,13 @@ debug = DebugToolbarExtension(app)
 def index():
     """Return homepage"""
 
-    #Creates list ['noun', 'verb']
-    part_of_speeches= silly_story.prompts
+    # Creates list ['noun', 'verb']
+    # TODO: part_of_speeches can be prompts
+    part_of_speeches = silly_story.prompts
 
+    # TODO: can just return here
+    html = render_template("questions.html", prompts = part_of_speeches) # TODO: can be prompts = prompts
 
-    html = render_template("questions.html", prompts = part_of_speeches)
     print("html is: ", html)
 
     return html
@@ -27,11 +29,15 @@ def get_madlibs():
 
     prompts = silly_story.prompts
     answers = {prompt:request.args[f"{prompt}"] for prompt in prompts}
+    print(request.args)
 
     print('This is prompts', prompts)
 
-    text = silly_story.get_result_text(answers)
+    # TODO: can just pass in request.args here
+    text = silly_story.get_result_text(request.args)
+    # text = silly_story.get_result_text(answers)
 
+    # TODO: rename to just text or story_text, not a template anymore
     return render_template("results.html", template=text)
 
 
