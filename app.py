db98d557fa1ca9e27bb9ede4f4eb86db84d02149
Carl Molina <carlandrew.molina@gmail.com>
2023-11-29 16:53:37 -0800

    Implemented first code review notes

--- app.py
+++ app.py
@@ -13,32 +13,17 @@ def index():
     """Return homepage"""
 
     # Creates list ['noun', 'verb']
-    # TODO: part_of_speeches can be prompts
-    part_of_speeches = silly_story.prompts
+    prompts = silly_story.prompts
 
-    # TODO: can just return here
-    html = render_template("questions.html", prompts = part_of_speeches) # TODO: can be prompts = prompts
-
-    print("html is: ", html)
-
-    return html
+    return render_template("questions.html", prompts = prompts)
 
 @app.get("/results")
 def get_madlibs():
     """Gets form results and return story madlib"""
 
-    prompts = silly_story.prompts
-    answers = {prompt:request.args[f"{prompt}"] for prompt in prompts}
-    print(request.args)
-
-    print('This is prompts', prompts)
-
-    # TODO: can just pass in request.args here
     text = silly_story.get_result_text(request.args)
-    # text = silly_story.get_result_text(answers)
 
-    # TODO: rename to just text or story_text, not a template anymore
-    return render_template("results.html", template=text)
+    return render_template("results.html", story_text=text)
 
 
 
